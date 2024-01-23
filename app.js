24868a4f34a3b84d689352b201a352f1e61242ec
MylaBhanuprasad <bhanu.myla39@gmail.com>
2024-01-23 16:31:32 +0530

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const http=require('http')
 
-const server=http.createServer()
-
-server.listen(4000,()=>{
-    console.log('bhanu')
+const server=http.createServer((req,res)=>{
+    res.setHeader('Content-Type','html')
+    res.write('<html>')
+    res.write('<head><title>First  page</title></head>')
+    res.write('<body><h1>Welcome to my Node Js project</h1></body>')
+    res.write('</html>')
 })
+
+server.listen(4000)
